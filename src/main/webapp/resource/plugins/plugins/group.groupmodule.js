f19a9673e48ed8eb38d30d3b228fa733565d8c63
vanwang <boyuan2000cn@163.com>
2013-09-22 16:02:45 +0800

    change

--- src/main/webapp/resource/plugins/plugins/group.groupmodule.js
+++ src/main/webapp/resource/plugins/plugins/group.groupmodule.js
@@ -1,5 +1,5 @@
 ;(function(){
-	"use strict"; // jshint ;_;
+	//"use strict"; // jshint ;_;
 	
 	$.group.groupmodule = {
 			url : "group.groupmodule.html",
@@ -12,12 +12,12 @@
 		var $content = $('#'+contentId);
 		if($content.length > 0){
 			//新建社团
-			var $toolbarButton1 = $('<button type="button" class="btn btn-success create-group"><span style="font-weight:bold;">+</span>&nbsp; 新增社团</button>').click(function(){
+			var $addNewGroupBtn = $('<button type="button" class="btn btn-success create-group"><span style="font-weight:bold;">+</span>&nbsp; 新增社团</button>').click(function(){
 				var id = $.util.generateRandomString(5);
 				var option = {
 					id: id,
-					url: 'test.html',
-					width: '650px',
+					url: 'group.groupmodule.new.html',
+					width: '550px',
 					callback: function($modal){$modal.on('click', '.save', function(){
 						alert("save");
 						$modal.find("button[data-dismiss='modal']").click();
@@ -26,25 +26,71 @@
 				$.ajaxModal(option);
 			});
 			
-			var $toolbarButton2 = $('<button type="button" class="btn btn-default create-group" style="margin-left:20px;">带条件刷新</button>').click(function(){
-				$(".puma-datagrid", $content).datagrid("reload");
-			});
+			var $deleteButton = $('<button class="btn btn-danger" type="button" style="margin-left:10px;margin-right:10px;">删除所选社团</button>').click(function(){
+				var selectedData = $(".group-datagrid", $content).datagrid("getChecked");
+				if(selectedData.length == 0){
+					alert("请选择要删除的社团!");
+					return;
+				}
+				bootbox.confirm("该操作会删除所选择的社团信息，确定要删除吗？", function(result) {
+						if(result == true){
+							alert("确认删除！");
+						}
+					});
+				}); 
 			
-			var $toolbarButton3 = $('<button type="button" class="btn btn-default create-group" style="margin-left:20px;">重新加载</button>').click(function(){
-				$(".puma-datagrid", $content).datagrid("load");
-			});
+			var $toolbar = [$addNewGroupBtn,$deleteButton];
 			
-			var $toolbar = [$toolbarButton1,$toolbarButton2,$toolbarButton3];
-			
-			$(".puma-datagrid", $content).datagrid({
+			$(".group-datagrid", $content).datagrid({
 				url:'testdata/group-list.json',
 				toolbar:$toolbar,
 				columns:[
 							{field:'name',title:'社团名称',width:'40%',formatter: function(value,row,index){
 								var $str = $('<button type="button" class="btn btn-default btn-noboder-inrow btn-noboder view-group">').text(value).click(function(){
 									var rowData = $(this).closest("tr").data("data");
-									//alert(rowData.id);
-									$(".puma-datagrid", $content).slidecontent("showsub");
+									$(".group-datagrid", $content).slidecontent({
+										url:'group.groupmodule.view.html',
+										queryParams:{},
+										callback:function($subitem){
+											
+											buildMemberMgtGrid($subitem);
+											
+											//检查是否有编辑权限
+											var $groupbasicinfoarea = $subitem.find(".groupbasicinfoarea");
+											if($groupbasicinfoarea.length > 0 && $groupbasicinfoarea.attr("data-editable") == "enable"){
+												$.fn.editable.defaults.mode = 'inline';
+												$.fn.editable.defaults.url = '/post'; 
+												$('.editable',$subitem).editable({
+														 url: 'updateGroupInfo.do',
+														 //name: 'groupname',
+														 //type:'text',
+														 //inputclass:'form-control',
+														 //pk:'id',
+														 success: function(data, config) {
+															 
+														 },
+														 error: function(errors) {
+															 console.log(errors);
+														 }
+												});
+											}
+											
+											//人员管理和资源管理的表格功能切换
+											$("input[type='radio'][name='grid-function']",$subitem).change( function() {
+												var val = $(this).val();
+												if(val == "membermgt"){
+													buildMemberMgtGrid($subitem);
+												}else if(val == "resourcemgt"){
+													buildResourceMgtGrid($subitem);
+												}
+											});
+											
+											$subitem.find(".btn-back").click(function(){
+												$(".puma-datagrid", $content).slidecontent("showmain");
+											});
+										}
+									});
+									$(".group-datagrid", $content).slidecontent("showsub");
 								});
 								
 								return $str;
@@ -53,154 +99,120 @@
 							{field:'guider',title:'辅导员'}
 						]
 			
-			}).slidecontent({
-				url:'slidecontent.html',
-				queryParams:{},
-				callback:function($subitem){
-					$subitem.find(".btn-back").click(function(){
-						$(".puma-datagrid", $content).slidecontent("showmain");
-					}).end().find(".demo-single-select-student").click(function(){
-						demoSelectStudent(true);
-					}).end().find(".demo-multi-select-student").click(function(){
-						demoSelectStudent(false);
-					}).end().find(".searchstudentinput").searchinput({
-						actionurl:"querystudent.do",
-						callback:function(formData){
-							console.log(formData);
-							return false;
-						}
-					}).end().find(".searchdormitoryinput").searchinput({
-						templateid:'template-search-dormitory-widget',
-						actionurl:"querydorm.do",
-						placement:"bottom-right",
-						callback:function(formData){
-							return false;
-						}
-					}).end().find(".demo-prompt-alert").click(function(){
-						bootbox.alert("操作成功!", function(result) {
-						}); 
-					}).end().find(".demo-prompt-confirm").click(function(){
-						bootbox.confirm("确定要删除吗？", function(result) {
-							alert("选择结果: "+result);
-						}); 
-					}).end().find(".demo-prompt-prompt").click(function(){
-						bootbox.prompt("随便输入点什么吧！", function(result) {
-							if (result === null) {
-								alert("啥都没输入呢！");
-							} else {
-								alert("Hi <b>"+result+"</b>");
-							}
-						});
-					}).end().find(".demo-prompt-custom").click(function(){
-						bootbox.dialog({
-							message: "这是个自定义对话框",
-							title: "自定义标题",
-							buttons: {
-								success: {
-								label: "Success!",
-								className: "btn-success",
-								callback: function() {
-										alert("great success");
-									}
-								},
-								danger: {
-									label: "Danger!",
-									className: "btn-danger",
-									callback: function() {
-										alert("Danger!");
-									}
-								},
-								main: {
-									label: "Click ME!",
-									className: "btn-primary",
-									callback: function() {
-										alert("Click ME!!");
-									}
-								}
-							}
-						});
-					}).end().find(".demo-loading-btn").click(function(){
-						$.loading("显示加载状态信息");
-					}).end().find(".demo-unloading-btn").click(function(){
-						$.unloading();
-					}).end().find(".demo-messaging-btn").click(function(){
-						$.messaging("显示自定义信息!");
-					}).end().find(".demo-success-messaging-btn").click(function(){
-						$.messaging("显示自定义信息!",true);
-					}).end().find(".demo-info-messaging-btn").click(function(){
-						$.messaging("显示自定义信息,显示自定义信息,显示自定义信息，显示自定义信息，显示自定义信息显示自定义信息!");
-					}).end().find(".tagselect").tagselect({
-						selectfun:function(){
-							demoTagSelectStudent($(this));
-							//$(this).tagselect("add",{value:'123123',text:'haha'});
-						}
-					}).end().find(".demo-check-tagselect-form").click(function(){
-						var formSerialize = $(this).closest('form').formSerialize();
-						alert(formSerialize);
-					});
-				}
 			});
-			
 		}
 	}
 	
-	function demoTagSelectStudent($target){
-		var option = {
-				url: 'selectstudent.html',
-				width: '400px',
-				callback: function($modal){
-						$modal.on('click', '.save', function(){
-							var selectedData = $(".puma-datagrid", $modal).datagrid("getChecked");
-							if(selectedData.length == 0){
-								alert("请选择学生!");
-								return false;
-							}else{
-								$(selectedData).each(function(){
-									$target.tagselect("add",{value:this.id,text:this.name});
-								});
-							}
-							$modal.find("button[data-dismiss='modal']").click();
-						});
-						
-						$(".searchinput", $modal).searchinput({
-							templateid:'template-search-student-widget',
-							actionurl:"querydorm.do",
-							placement:"bottom-right",
-							callback:function(formData){
-								console.log(formData);
-								updateDatagrid(formData);
-								return true;
-							}
-						});
-						
-						var $datagrid = $(".puma-datagrid", $modal);
-						
-						function updateDatagrid(formData){
-							$datagrid.datagrid({
-								url:'testdata/student-search-list.json',
-								queryParams:formData
-							});
-							//$datagrid.datagrid("reload", formData);
-						}
-						
-						$datagrid.datagrid({
-							//url:'testdata/student-search-list.json',
-							showBottomPagination:false,
-							singleSelect:false,
-							columns:[
-										{field:'name',title:'姓名',width:'40%'},
-										{field:'student-no',title:'学号'}
-									]
-						
-						});
-						
-				}
+	function buildMemberMgtGrid($subitem){
+		var $addNewMemberBtn = $('<button type="button" class="btn btn-success create-group"><span style="font-weight:bold;">+</span>&nbsp; 新增社团成员</button>').click(function(){
+			var $datagrid = $(".group-member-datagrid", $subitem);
+			
+			groupAddStudent($datagrid);
+			/*var id = $.util.generateRandomString(5);
+			var option = {
+				id: id,
+				url: 'group.groupmodule.new.html',
+				width: '550px',
+				callback: function($modal){$modal.on('click', '.save', function(){
+					alert("save");
+					$modal.find("button[data-dismiss='modal']").click();
+				});}
 			};
-		$.ajaxModal(option);
+			$.ajaxModal(option);*/
+		});
+		
+		var $deleteMemberButton = $('<button class="btn btn-danger" type="button" style="margin-left:10px;margin-right:10px;">删除社团成员</button>').click(function(){
+			var selectedData = $(".group-member-datagrid", $subitem).datagrid("getChecked");
+			if(selectedData.length == 0){
+				alert("请选择要删除的人员!");
+				return;
+			}
+			bootbox.confirm("该操作会删除所选择的社团人员，确定要删除吗？", function(result) {
+					if(result == true){
+						alert("确认删除！");
+					}
+				});
+			}); 
+		
+		var $toolbar = [$addNewMemberBtn,$deleteMemberButton];
+		
+		$subitem.find(".group-member-datagrid").datagrid({
+			url:'testdata/group-student-search-list.json',
+			toolbar:$toolbar,
+			columns:[
+						{field:'name',title:'姓名',width:'100px'},
+						{field:'student-no',title:'学号',width:'100px'},
+						{field:'title',title:'职务',width:'100px',formatter:function(value,row,index){
+							return $('<div data-type="select" class="editable editable-click"></div>').text(row.title).editable({
+								url: 'updateGroupPosition.do',
+								pk: row.id,
+								name:"position",
+						        prepend: [
+						                  {value: 0, text: '无职位'},
+						              ],
+						        showbuttons: false,
+						        source:"testdata/group-position.json",
+						        success: function(data, config) {
+									 
+								 },
+								 error: function(errors) {
+									 console.log(errors);
+								 }
+						    }); 
+						}},
+						{field:'join-time',title:'加入时间'}
+					]
+		
+		});
 	}
 	
+	function buildResourceMgtGrid($subitem){
+		var $addNewResourceBtn = $('<button type="button" class="btn btn-success create-group"><span style="font-weight:bold;">+</span>&nbsp; 新增社团资源</button>').click(function(){
+			var id = $.util.generateRandomString(5);
+			var option = {
+				id: id,
+				url: 'group.groupmodule.view.addresource.html',
+				width: '550px',
+				callback: function($modal){$modal.on('click', '.save', function(){
+					alert("save");
+					
+					//新增资源后关闭模态窗口
+					$modal.find("button[data-dismiss='modal']").click();
+					//并刷新资源列表
+					$subitem.find(".group-member-datagrid").datagrid("load");
+				});}
+			};
+			$.ajaxModal(option);
+		});
+		
+		var $deleteResourceButton = $('<button class="btn btn-danger" type="button" style="margin-left:10px;margin-right:10px;">删除社团资源</button>').click(function(){
+			var selectedData = $(".group-member-datagrid", $subitem).datagrid("getChecked");
+			if(selectedData.length == 0){
+				alert("请选择要删除的资源!");
+				return;
+			}
+			bootbox.confirm("该操作会删除所选择的资源，确定要删除吗？", function(result) {
+					if(result == true){
+						alert("确认删除！");
+					}
+				});
+			}); 
+		
+		var $toolbar = [$addNewResourceBtn,$deleteResourceButton];
+		
+		$subitem.find(".group-member-datagrid").datagrid({
+			url:'testdata/group-resource-list.json',
+			toolbar:$toolbar,
+			columns:[
+						{field:'name',title:'资源名称',width:'40%'},
+						{field:'member',title:'发布人'},
+						{field:'datetime',title:'发布时间'}
+					]
+		
+		});
+	}
 	
-	function demoSelectStudent(isSingleSelect){
+	function groupAddStudent($memberdatagrid){
 		var option = {
 				url: 'selectstudent.html',
 				width: '400px',
@@ -211,6 +223,34 @@
 								alert("请选择学生!");
 							}
 							console.log(selectedData);
+							//TODO ajax添加学生操作
+							var queryParams = "ids=";
+							var i = 0;
+							$(selectedData).each(function(){
+								queryParams += (i==0 ? this.id:","+this.id);
+								i++;
+							});
+							$.loading("正在新增社团成员...");
+							$.ajax( {
+						        "dataType": 'JSON',
+						        "type": "POST",
+						        "url": "addGroupMember.do",
+						        "data": queryParams,//example: ids=a09sdf,isdfiajs,asjsdfak,akdsfa
+						        "contentType": "application/x-www-form-urlencoded; charset=UTF-8",
+						        "success": function(data){
+						        	$.unloading();
+						        	//添加完学生后，刷新学生列表
+									$memberdatagrid.datagrid("load");
+									//并关闭选择窗口
+									$modal.find("button[data-dismiss='modal']").click();
+						        },
+						        "error":function(data){
+						        	$.unloading();
+						        	alert("添加社团成员失败,请重试！");
+						        	return;
+								}
+						      } );
+							
 							//$modal.find("button[data-dismiss='modal']").click();
 						});
 						
@@ -237,8 +277,6 @@
 						
 						$datagrid.datagrid({
 							//url:'testdata/student-search-list.json',
-							showBottomPagination:false,
-							singleSelect:isSingleSelect,
 							columns:[
 										{field:'name',title:'姓名',width:'40%'},
 										{field:'student-no',title:'学号'}
